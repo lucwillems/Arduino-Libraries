ba1bf1656e18a33d38c85586a246977a9619faec
luc <luc.willems@it4y.eu>
2015-05-02 20:11:37 +0200

    - initial import

---
+++ Analog/HardwareAnalog.cpp
@@ -0,0 +1,229 @@
+#include <Arduino.h>
+#include <HardwareAnalog.h>
+
+// Define cbi() and sbi() for clearing and setting bits in the
+// ADC registers.
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+HardwareAnalog::HardwareAnalog() {
+  analogRef = DEFAULT;
+  prescaler = PS_128;
+  sampleCount = 0; 
+  currentChannel=0;
+}
+
+void HardwareAnalog::setPrescaler(byte x) {
+#ifdef SERIAL_DEBUG
+  Serial.print("ADC prescaler=");
+  Serial.println(x,HEX);
+#endif
+  prescaler = x & 0x07;
+}
+
+void HardwareAnalog::setReference(byte reference) {
+#ifdef SERIAL_DEBUG
+  Serial.print("ADC reference=");
+  Serial.println(reference,HEX);
+#endif
+  analogRef=reference & 0x03;
+}
+
+void HardwareAnalog::setSamples(byte count) {
+#ifdef SERIAL_DEBUG
+  Serial.print("ADC samples=");
+  Serial.println(count,HEX);
+#endif
+  sampleCount=count;
+}
+  
+void HardwareAnalog::setup(byte n, const byte analogChannels[]) {
+  nrOfChannels=min(MAX_CHANNELS,n);
+#ifdef SERIAL_DEBUG
+  Serial.println("ADC setup ");
+  Serial.println(nrOfChannels);
+#endif
+  for (int i=0; i < nrOfChannels; ++i) {
+    values[i]=0;
+    channels[i] = analogChannels[i];
+    if (channels[i] >= ADC_ARDUINO_A0) {
+      //When using A0,A1... calculate based on A0=0x00 ... A6=0x07 
+      channels[i]=channels[i]-ADC_ARDUINO_A0;
+    }
+  } 
+  ADCinit();
+}
+
+void HardwareAnalog::ADCinit() {
+#ifdef SERIAL_DEBUG
+  Serial.println("init ADC");
+#endif
+// set up the ADC
+  ADCSRA &= ~PS_128;  // remove bits set by Arduino library
+  // you can choose a prescaler from above.
+  // PS_16, PS_32, PS_64 or PS_128
+  ADCSRA |= prescaler;    // set our own prescaler to 64 
+  cbi(ADMUX, ADLAR); // Make sure the ADC value is right-justified.
+
+  sbi(ADCSRA, ADEN);      // Enable the ADC.
+  delay(1);
+  sbi(ADCSRA, ADIE); // Enable ADC complete interrupts.
+  
+}
+
+int HardwareAnalog::analogRead(byte channel) {
+  if (channel < nrOfChannels) {
+     return values[channel];
+  }
+  return -1;
+}
+
+unsigned long HardwareAnalog::analogTime(byte channel) {
+  if (channel < nrOfChannels) {
+     return times[channel];
+  }
+  return -1;
+}
+
+unsigned long HardwareAnalog::scanTime() {
+  unsigned long time=0;
+  for (int i=0;i<nrOfChannels;i++) {
+      time += times[i];
+  }
+  return time;
+}
+  
+unsigned long HardwareAnalog::Count() {
+  return scanCounter;
+}
+
+boolean HardwareAnalog::startScan() {
+  //is there a scan active, if so,we can't start a new one
+  if (scanActive) {
+     return false;
+  }
+#ifdef SERIAL_DEBUG
+  Serial.println("start scan");
+#endif
+  scanActive=true;
+  currentChannel=0;
+  currentSample=sampleCount;
+  ADCstart(); //start the scan
+  return true;
+}
+
+bool HardwareAnalog::isScanActive() {
+  return scanActive;
+}
+
+void HardwareAnalog::ADCstart() {
+   //Selecteer the channel
+  byte c=channels[currentChannel];//calculate the channel
+  byte mux=analogRef<<6 | c &0x0f;//default to channel 0;
+  
+#ifdef SERIAL_DEBUG
+  Serial.print("ADC start: channel=");
+  Serial.print(currentChannel);
+  Serial.print(" mux=");
+  Serial.println(mux,HEX);
+#endif
+   //set AD mux channel and reference
+   ADMUX=mux;
+   sbi(ADCSRA, ADSC); // Start the ADC conversion.
+   startADCTime=micros();
+}
+
+void HardwareAnalog::idle() {
+#ifdef SERIAL_DEBUG
+  Serial.println("idle");
+#endif
+}
+
+void HardwareAnalog::ADCinitInternal(byte mux) {
+  ADMUX=mux; //select ref=1v1 and internal tempureture
+  cbi(ADCSRA ,ADIE);  // DISABLE ADC complete interrupts,we will poll
+  sbi(ADCSRA ,ADPS2); //set 128 prescale
+  sbi(ADCSRA ,ADPS1);
+  sbi(ADCSRA ,ADPS0);
+  sbi(ADCSRA ,ADEN);  // Enable the ADC
+  delay(10);
+  sbi(ADCSRA ,ADSC);  // Start the ADC conversion
+}
+
+int HardwareAnalog::scanInternal(byte n,byte mux) {
+  //we can't run scans and this together
+  //we should have some timeout here ?
+  while(scanActive) { idle(); }
+  scanActive=1; //block any new scan until we are finished
+  ADCinitInternal(mux);
+  //start manual scan
+  byte samples=n;      // we take average of 200 samples
+  long value=0;
+  while (samples>0) {
+     while(ADCSRA & (1 << ADSC)); // waits for the ADC to finish 
+     samples--;
+     value +=ADCW;  //read value    
+  }
+  ADCinit();
+  scanActive=0;
+  return (int)(value/(long)n);
+}
+
+int HardwareAnalog::Temperature(byte n) {
+  return scanInternal(n,0xC8);
+}
+
+int HardwareAnalog::internalReference() {
+  return scanInternal(5,0xCE);
+}
+
+int HardwareAnalog::internalGND() {
+  return scanInternal(5,0xCF);
+}
+
+void HardwareAnalog::ADC_ISR() {
+#ifdef SERIAL_DEBUG
+  Serial.print("ADC ISR: channel=");
+  Serial.print(currentChannel);
+  Serial.print(" sample= ");
+  Serial.println(currentSample);
+#endif
+  currentSample--;
+  if (currentSample == 0) {
+      #ifdef SERIAL_DEBUG
+        Serial.println("ADC ISR: sample finished");
+      #endif
+      unsigned long time=micros();
+      times[currentChannel]=time-startADCTime;
+      values[currentChannel]=ADCW; //read ADC as word,take care of ADCL/ADCH order
+      //start next conversion if currentChannel < nrOfChannels;
+      if (++currentChannel >= nrOfChannels) {
+         scanCounter++;
+         #ifdef SERIAL_DEBUG
+           Serial.print("ADC ISR: scan finished :");
+           Serial.println(scanCounter);
+         #endif
+         scanActive=false;
+         return;
+      } else {
+	 //start new set of samples
+	 currentSample=sampleCount;
+      }
+  } else {
+    int tmp=ADCW; //read ADC but throw it away
+  }
+  //Start next channel scan
+  ADCstart();
+}
+
+// Interrupt service function when ADC is finished
+ISR(ADC_vect) {
+   Analog.ADC_ISR();
+}
+
+//Singleton class to use with sketches
+HardwareAnalog Analog;
---
+++ Analog/HardwareAnalog.h
@@ -0,0 +1,95 @@
+#ifndef ADC_h
+#define ADC_h
+
+#include "Arduino.h"
+
+//#define SERIAL_DEBUG 
+
+// Define various ADC prescaler
+const byte PS_2  =  (1 << ADPS0);
+const byte PS_4  =  (1 << ADPS1);
+const byte PS_8  =  (1 << ADPS1) | (1 << ADPS0);
+const byte PS_16  = (1 << ADPS2);
+const byte PS_32  = (1 << ADPS2) | (1 << ADPS0);
+const byte PS_64  = (1 << ADPS2) | (1 << ADPS1);
+const byte PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+const byte MAX_CHANNELS = 5;
+const byte ADC_A0=0x00;
+const byte ADC_A1=0x01;
+const byte ADC_A2=0x02;
+const byte ADC_A3=0x03;
+const byte ADC_A4=0x04;
+const byte ADC_A5=0x05;
+const byte ADC_A6=0x06;
+const byte ADC_A7=0x07;
+
+const byte ADC_ARDUINO_A0=14;
+const byte ADC_ARDUINO_A1=15;
+const byte ADC_ARDUINO_A2=16;
+const byte ADC_ARDUINO_A3=17;
+const byte ADC_ARDUINO_A4=18;
+const byte ADC_ARDUINO_A5=19;
+const byte ADC_ARDUINO_A6=20;
+const byte ADC_ARDUINO_A7=22;
+
+//const byte ADC_MAX=0x1F; //max channels
+
+class HardwareAnalog {
+
+ private:
+  //settings
+  byte analogRef;
+  byte prescaler;
+  byte sampleCount;  
+
+  //status counters/flags  
+  unsigned long scanCounter;
+  bool scanActive;
+
+  unsigned long startADCTime;
+  
+  volatile int values[MAX_CHANNELS];
+  volatile byte channels[MAX_CHANNELS];
+  volatile unsigned long times[MAX_CHANNELS];
+  volatile byte currentChannel;
+  volatile byte currentSample;
+  volatile byte nrOfChannels;
+
+  void ADCinit();
+  inline void ADCstart();
+
+  void ADCinitInternal(byte mux);
+  int  scanInternal(byte n,byte mux);
+  
+public:
+ 
+  void setup(byte n, const byte channels[]);
+  void setPrescaler(byte x);
+  void setReference(byte reference);
+  void setSamples(byte count);
+  
+  int analogRead(byte channel);
+  int Temperature(byte samples);
+  int internalReference();
+  int internalGND();
+  
+  unsigned long analogTime(byte channel);
+  unsigned long scanTime();
+  unsigned long Count();
+
+  void idle();
+  
+  bool startScan();
+  bool isScanActive();
+  
+  inline HardwareAnalog();
+  inline void ADC_ISR();
+  
+};
+
+extern HardwareAnalog Analog;  
+
+
+
+#endif 
